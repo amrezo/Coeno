7d2707b1f775f921076bc686fb2cb70fe2ccc956
Amr Al-Refae <amr-al-refae@Amrs-Mac-mini.local>
2019-06-24 09:05:33 -0400

    Setup login system and account update

---
+++ coeno/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a969af95d4d5dc2d2bba0cb3f0081156'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from coeno import routes
---
+++ run.py
@@ -0,0 +1,4 @@
+from coeno import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
