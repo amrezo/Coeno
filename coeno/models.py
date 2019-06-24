ec2d3bad3807fea39cfc71866ec4334ac5cff33b
Amr Al-Refae <amr-al-refae@Amrs-Mac-mini.local>
2019-06-24 16:44:44 -0400

    Tweak edit account page and setup homepage

--- coeno/models.py
+++ coeno/models.py
@@ -23,6 +23,9 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    view_count = db.Column(db.Integer, nullable=True, default=0)
+    like_count = db.Column(db.Integer, nullable=True, default=0)
+    comment_count = db.Column(db.Integer, nullable=True, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
--- coeno/routes.py
+++ coeno/routes.py
@@ -12,13 +12,55 @@ posts = [
         'author': 'Corey Schafer',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'April 20, 2018',
+        'view_count': 230,
+        'like_count': 12,
+        'comment_count': 25
     },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'April 21, 2018',
+        'view_count': 230,
+        'like_count': 12,
+        'comment_count': 25
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+        'view_count': 230,
+        'like_count': 12,
+        'comment_count': 25
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+        'view_count': 230,
+        'like_count': 12,
+        'comment_count': 25
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+        'view_count': 230,
+        'like_count': 12,
+        'comment_count': 25
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+        'view_count': 230,
+        'like_count': 12,
+        'comment_count': 25
     }
 ]
 
