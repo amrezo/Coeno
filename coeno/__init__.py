b58f760fb5626a8f5bdeb47e3f43d22be9ae4f6f
Amr Al-Refae <amrezo94@gmail.com>
2019-11-21 09:40:14 -0500

    Update PostgresSQL URI

--- coeno/__init__.py
+++ coeno/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a969af95d4d5dc2d2bba0cb3f0081156'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://amr:8kJTh4XOA2O4DPmaa7vpi0rEAxerGi0u@postgres.render.com/coeno_5wj1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://amr:vJWfUH88htaK0bTbU6QOwpC0XTIyD6IG@postgres.render.com/coeno_hbht'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
